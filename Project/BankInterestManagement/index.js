11d2febd2cb99a4d7d233a6ea05b2bacb5bad8e0
AkShay Kulkarni <akshaykulkarni8496@gmail.com>
2022-05-12 16:15:41 +0530

    Events Handler

--- Project/BankInterestManagement/index.js
+++ Project/BankInterestManagement/index.js
@@ -1,32 +1,53 @@
-// let bankBalance1 = Number(prompt("Enter the first bank balance"))
-// let bankBalance2 = Number(prompt("Enter the second bank balance"))
-// let bankBalance3 = Number(prompt("Enter the third bank balance"))
-// let bankBalance4 = Number(prompt("Enter the forth bank balance"))
-// let bankBalance5 = Number(prompt("Enter the fifth bank balance"))
+/*  
+ Take input of bank balances of 5 acc. holder as input
+
+ if balance > 2L => add interest  of 10%  and deduct 0.2% maintainance charge
+
+ if balance > 1L => add interest  of 5%  and deduct 0.05% maintainance charge
+
+ if any balance less than or equal => add 4% interest
+
+ log/alert updated arra of balance
+*/
+
+// let bankBalance1 = document.getElementById('bank-balance-1').value
+// console.log(bankBalance1);
 
 
-let bankBalances = Number(prompt('Enter all bank balances seperated by a ", " '))
 
-console.log(bankBalances.split(','));
-// let bankBalance1s = [249389, 34561, 3462, 242442, 100001]
 
-for( let i=0; i< bankBalances.length; i++){
-    if(bankBalances[i] > 200000){
-        bankBalances[i] = Number(bankBalances[i])
-        bankBalances[i] = bankBalances[i] + 0.1 * bankBalances[i]  // 0.1 = 10/100
-        bankBalances[i] = bankBalances[i] - 0.002 * bankBalances[i] //0.002 = 0.2/100
+// // let bankBalances = prompt('Enter all bank balances seperated by a "," ').split(',')
+// //100001,120000,210000,1000,300000
 
-    }
-    // // else if(bankBalances[i] > 100000){
-    //     bankBalances[i] = Number(bankBalances[i])
-    //     bankBalances[i] = bankBalances[i] + 0.05 * bankBalances[i]  // 0.05 = 5/100
-    //     bankBalances[i] = bankBalances[i] - 0.0005 * bankBalances[i] //0.0005 = 0.05/100
+// // let bankBalances = [249389, 34561, 3462, 242442, 100001]
 
-    // }
-    // else{
-    //     bankBalances[i] = Number(bankBalances[i])
-    //     bankBalances[i] = bankBalances[i] + 0.04 * bankBalances[i] 
-    // }
+// for( let i=0; i< bankBalances.length; i++){
+//     bankBalances[i] = Number(bankBalances[i])
+
+//     if(bankBalances[i] > 200000){
+//         bankBalances[i] = bankBalances[i] + 0.1 * bankBalances[i]  // 0.1 = 10/100
+//         bankBalances[i] = bankBalances[i] - 0.002 * bankBalances[i] //0.002 = 0.2/100
+
+//     }
+//     else if(bankBalances[i] > 100000){
+//         bankBalances[i] = bankBalances[i] + 0.05 * bankBalances[i]  // 0.05 = 5/100
+//         bankBalances[i] = bankBalances[i] - 0.0005 * bankBalances[i] //0.0005 = 0.05/100
+//     }
+//     else{
+//         bankBalances[i] = Number(bankBalances[i])
+//         bankBalances[i] = bankBalances[i] + 0.04 * bankBalances[i] 
+//     }
+    
+// }
+
+// console.log(bankBalances) 
+
+
+function CalculateInterest(){
+    let bankBalance1 = Number(document.getElementById('bank-balance-1').value)
+    let bankBalance2 = number(document.getElementById('bank-balance-2').value)
+    let bankBalance3 = Number(document.getElementById('bank-balance-3').value)
+
+    console.log(bank-balance-1,bank-balance-2,bank-balance-3);
+    
 }
-
-console.log(bankBalances);
