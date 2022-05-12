9952c5321e4b941faccc3cc9c137ef8f7527ab23
AkShay Kulkarni <akshaykulkarni8496@gmail.com>
2022-05-12 19:39:34 +0530

    Bank Interest Management Complete

--- Project/BankInterestManagement/index.js
+++ Project/BankInterestManagement/index.js
@@ -45,9 +45,38 @@
 
 function CalculateInterest(){
     let bankBalance1 = Number(document.getElementById('bank-balance-1').value)
-    let bankBalance2 = number(document.getElementById('bank-balance-2').value)
+    let bankBalance2 = Number(document.getElementById('bank-balance-2').value)
     let bankBalance3 = Number(document.getElementById('bank-balance-3').value)
 
-    console.log(bank-balance-1,bank-balance-2,bank-balance-3);
-    
+    // console.log(bankBalance1,bankBalance2,bankBalance3);
+    let updatedBankBalance1 = document.getElementById('updated-bank-balance-1')
+    let updatedBankBalance2 = document.getElementById('updated-bank-balance-2')
+    let updatedBankBalance3 = document.getElementById('updated-bank-balance-3')
+    let bankBalances = [bankBalance1, bankBalance2, bankBalance3]
+
+    // console.log(bankBalances);
+
+        for( let i=0; i< bankBalances.length; i++){
+            bankBalances[i] = Number(bankBalances[i])
+        
+            if(bankBalances[i] > 200000){
+                bankBalances[i] = bankBalances[i] + 0.1 * bankBalances[i]  // 0.1 = 10/100
+                bankBalances[i] = bankBalances[i] - 0.002 * bankBalances[i] //0.002 = 0.2/100
+        
+            }
+            else if(bankBalances[i] > 100000){
+                bankBalances[i] = bankBalances[i] + 0.05 * bankBalances[i]  // 0.05 = 5/100
+                bankBalances[i] = bankBalances[i] - 0.0005 * bankBalances[i] //0.0005 = 0.05/100
+            }
+            else{
+                bankBalances[i] = Number(bankBalances[i])
+                bankBalances[i] = bankBalances[i] + 0.04 * bankBalances[i] 
+            }
+            
+        }
+
+        // console.log(bankBalances);
+        updatedBankBalance1.value = bankBalances[0]
+        updatedBankBalance2.value = bankBalances[1]
+        updatedBankBalance3.value = bankBalances[2]
 }
